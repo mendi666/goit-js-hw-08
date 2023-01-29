e8131f8d803f96aa7d67adbddc18b289751fa6d2
mendi666 <113132777+mendi666@users.noreply.github.com>
2023-01-29 19:31:28 +0200

    PUSH

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,19 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = {
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const timeData = {
   duration: 61.857,
   percent: 0.049,
   seconds: 3.034,
 };
 
-player.on('timeupdate', function (onPlay) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(onPlay.seconds)
-  );
-});
+function onPlay(timeData) {
+  localStorage.setItem('videoplayer-current-time',timeData.seconds);
+}
 
-const savedData = localStorage.getItem('videoplayer-current-time');
-const parsedData = JSON.parse(savedData);
-
-window.addEventListener('load', event => {
-  player.setCurrentTime(parsedData);
-});
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form')
+const inputRef = document.querySelector('.feedback-form input')
+const textareaRef = document.querySelector('.feedback-form textarea')
+
+const formData = {}
+
+formRef.addEventListener('submit', throttle(onFormSubmit, 500))
+formRef.addEventListener('input', e =>{
+    formData[e.target.name] = e.target.value
+    console.log(formData)
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+})
+
+const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'))
+
+console.log(parsedData)
+
+function inputAdd () {
+    if (parsedData != null) {
+        inputRef.value = parsedData.email
+        textareaRef.value = parsedData.message}
+}
+
+inputAdd()
+
+function onFormSubmit(e) {
+e.preventDefault();
+e.currentTarget.reset()
+localStorage.clear()
+}
 
